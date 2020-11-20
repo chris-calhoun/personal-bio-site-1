667d730eb282b6e83a18060482f03092bae1b1dd
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-20 00:03:30 -0600

    displayed images stored in firebase storage.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,11 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
 import Views from './helpers/viewHelper';
 import Navbar from './components/navbar/navbar';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   Navbar.addNavbar();
   Views.viewListener('home-link');
 };
